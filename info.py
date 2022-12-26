e053b892852bfdb54e8c61a95d0f71b44c08e8c6
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2022-12-26 09:41:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -38,7 +38,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001598034869))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'mallufiles)
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'mallufiles')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
